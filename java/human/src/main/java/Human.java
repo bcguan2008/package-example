0f7b421fe8a3751712c9b3549ca6dba40a32d9d3
ben.b.guan <bingchang.guan01@liulishuo.com>
2020-09-17 16:11:13 +0800

    feat: init v2 human

--- java/human/src/main/java/Human.java
+++ java/human/src/main/java/Human.java
@@ -3,7 +3,10 @@ package com.laix.human;
 public class Human {
     
     public String sayHelloWolrd(){
-        return "()S*)*)(!)(@)*)!";
+        return "Hello World!";
     }
 
+    public void useTool(){
+        System.out.println("i can use some tools!");
+    }
 }
