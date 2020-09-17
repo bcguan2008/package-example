cac0caf64ca1d39fe78f4478343a61442ccdecb1
ben.b.guan <bingchang.guan01@liulishuo.com>
2020-09-17 19:28:48 +0800

    fix: v2 update

--- java/human/src/main/java/Human.java
+++ java/human/src/main/java/Human.java
@@ -6,12 +6,8 @@ public class Human {
         return "Hello World!";
     }
 
-    public void useTool(){
-        System.out.println("i can use some tools!");
-    }
 
     public static void main(String[] args){
         System.out.println(new Human().sayHelloWorld());
-        new Human().useTool();
     }
 }
