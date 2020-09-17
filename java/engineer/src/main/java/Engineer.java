9b7e412c92586ab8bca814371ef2b546962b4d73
ben.b.guan <bingchang.guan01@liulishuo.com>
2020-09-17 17:12:26 +0800

    feat: init engineer v1

---
+++ java/engineer/src/main/java/Engineer.java
@@ -0,0 +1,11 @@
+package com.laix.engineer;
+
+import com.laix.human.Human;
+
+public class Engineer extends Human{
+    
+    public void coding(){
+        System.out.println("i am coding now!");
+    }
+
+}
