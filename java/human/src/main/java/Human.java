f24bc5d9a4aca916342ec22a1409cfac6b4f79e9
ben.b.guan <bingchang.guan01@liulishuo.com>
2020-09-17 17:44:10 +0800

    fix: update function name

--- java/human/src/main/java/Human.java
+++ java/human/src/main/java/Human.java
@@ -2,8 +2,12 @@ package com.laix.human;
 
 public class Human {
     
-    public String sayHelloWolrd(){
+    public String sayHelloWorld(){
         return "()S*)*)(!)(@)*)!";
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Human().sayHelloWorld());
     }
 
 }
