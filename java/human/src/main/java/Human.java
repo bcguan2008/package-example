844eba705cc327e9d9c303fc8b93db1eb5763c6e
ben.b.guan <bingchang.guan01@liulishuo.com>
2020-09-17 17:47:36 +0800

    fix: update function name

--- java/human/src/main/java/Human.java
+++ java/human/src/main/java/Human.java
@@ -2,11 +2,16 @@ package com.laix.human;
 
 public class Human {
     
-    public String sayHelloWolrd(){
+    public String sayHelloWorld(){
         return "Hello World!";
     }
 
     public void useTool(){
         System.out.println("i can use some tools!");
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Human().sayHelloWorld());
+        new Human().useTool();
     }
 }
